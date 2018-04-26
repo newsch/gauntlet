e03d0f1259858d50ff595b8895d820dcbd8d52d3
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2018-04-26 12:04:02 -0400

    Implement Exercise 3

--- ransac.m
+++ ransac.m
@@ -1,6 +1,8 @@
-testRobustLineFit()
+% testgetDist()
+load scan4.mat
+robustLineFit(theta,r,1,20)
 
-function testRobustLineFit()
+function testgetDist()
     p1 = [0 0];
     p2 = [1 1];
     p3 = [0 1];
@@ -11,31 +13,52 @@ function testRobustLineFit()
     p3 = [2 0];
     getDist(p1,p2,p3)  % should be 1
     
+    p1 = [0 0];
+    p2 = [1 1];
+    p3 = [0 1; 1 0; 2 2];
+    getDist(p1,p2,p3)
 end
 
 function robustLineFit(theta, r, d, n)
 %ROBUSTLINEFIT  Fit lines to scanner data with RANSAC
     [ctheta, cr] = cleanData(theta,r);
-    data = polar2cart(ctheta,cr);
+    [x,y] = polar2cart(deg2rad(ctheta),cr);
+    data = [x y];
     choices = randsample(length(data),2*n);
-    for i = 1:2:length(choices)
-        p1 = data(choices(i),:)
-        p2 = data(choices(i+1),:)
+    tic;
+    for i = 2:2:length(choices)
+        p1 = data(choices(i-1),:)
+        p2 = data(choices(i),:)
+        D = getDist(p1,p2,data);  % get distances of all data points to line
+        results(i / 2) = sum(D > d);
     end
-   
+    toc
+    [sR, sI] = sort(results,'descend');
+    sP2 = data(choices(sI*2),:);
+    sP1 = data(choices(sI*2 - 1),:);
+    
+    figure; hold on
+    plot(data(:,1),data(:,2),'ks')  % cleaned lidar data
+    for i = 1:3
+        quiver(sP1(i,1),sP1(i,2),sP2(i,1)-sP1(i,1),sP2(i,2)-sP1(i,2),'LineWidth',2)
+    end
+    legend('data','1','2','3')
 end
 
 function dist = getDist(p1,p2,p3)
 % GETDIST  finds the perpendicular distance from p3 to the line
-% through p1 and p2
-    points = num2cell([p1 p2 p3]);
-    [x1,y1,x2,y2,x0,y0] = deal(points{:});
-    dist = abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*y1) / sqrt((y2-y1)^2 + (x2-x1)^2);
-            
+% through p1 and p2.
+% p3 is a collection of x and y points of format [X Y]
+    
+    line_points = num2cell([p1 p2]);
+    [x1,y1,x2,y2] = deal(line_points{:});
+    x0 = p3(:,1);
+    y0 = p3(:,2);
+    dist = abs((y2-y1).*x0 - (x2-x1).*y0 + x2.*y1 - y2.*y1) / sqrt((y2-y1).^2 + (x2-x1).^2);
 end
 
 function [ctheta,cr] = cleanData(theta, r)
-    i_clean = r > 0; % indices of clean data
+    i_clean = r ~= 0; % indices of clean data
     ctheta = theta(i_clean);
     cr = r(i_clean);
 end
