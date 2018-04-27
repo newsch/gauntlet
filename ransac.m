5b12c17604b4de5c25e83f965f3ee43daa338873
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2018-04-26 21:48:35 -0400

    Implement Exercise 4
    - Factor data cleaning/conversion out of robustLineFit
    
    - Clean dataset of far-away points
    
    - Repeat line-fitting on dataset
    
    - Use playpen data

--- ransac.m
+++ ransac.m
@@ -1,7 +1,29 @@
 % testgetDist()
-load scan4.mat
-
-robustLineFit(theta,r,0.2,40);
+load playpensample.mat
+[ctheta, cr] = cleanData(theta,r);
+[x,y] = polar2cart(deg2rad(ctheta),cr);
+figure; hold on
+plot(x,y,'ks')
+plot(0,0,'kO')
+legendata = ["lidar data","neato location"];
+data = [x y];
+for i = 1:10
+    if length(data) < 4
+        fprintf("Ended after %d iterations.\n",i-1)
+        break
+    end
+    [p1,p2,in,out] = robustLineFit(data(:,1),data(:,2),0.1,floor(length(data)/4));
+    quiver(p1(1),p1(2),p2(1)-p1(1),p2(2)-p1(2),'LineWidth',2)
+    plot(in(:,1),in(:,2),'*')
+    data = out;
+    legendata = [legendata, sprintf("round %d line",i)];
+    legendata = [legendata, sprintf("round %d inliers",i)];
+end
+legend(legendata)
+axis equal
+xlabel 'x pos (meters)'
+ylabel 'y pos (meters)'
+title 'RANSAC Implementation'
 
 function testgetDist()
     p1 = [0 0];
@@ -20,10 +42,8 @@ function testgetDist()
     getDist(p1,p2,p3)
 end
 
-function [p1,p2,subset] = robustLineFit(theta, r, d, n)
+function [p1,p2,inliers,outliers] = robustLineFit(x, y, d, n)
 %ROBUSTLINEFIT  Fit lines to scanner data with RANSAC
-    [ctheta, cr] = cleanData(theta,r);
-    [x,y] = polar2cart(deg2rad(ctheta),cr);
     data = [x y];
     choices = randsample(length(data),2*n);
     for i = 2:2:length(choices)
@@ -36,18 +56,18 @@ function [p1,p2,subset] = robustLineFit(theta, r, d, n)
     [sR, sI] = sort(results.inliers,'descend');
     sP2 = data(choices(sI*2),:);
     sP1 = data(choices(sI*2 - 1),:);
-    figure; hold on
-    plot(data(:,1),data(:,2),'ks')  % cleaned lidar data
-    for i = 1:3
-        quiver(sP1(i,1),sP1(i,2),sP2(i,1)-sP1(i,1),sP2(i,2)-sP1(i,2),'LineWidth',2)
-    end
-    legend('data','line 1','line 2','line 3')
+%     figure; hold on
+%     plot(data(:,1),data(:,2),'ks')  % cleaned lidar data
+%     for i = 1:3
+%         quiver(sP1(i,1),sP1(i,2),sP2(i,1)-sP1(i,1),sP2(i,2)-sP1(i,2),'LineWidth',2)
+%     end
+%     legend('data','line 1','line 2','line 3')
     
     p1 = sP1(1,:);
     p2 = sP2(1,:);
     D = results.distances(:,sI(1));
-    subset = data(D < d,:);
-    plot(subset(:,1),subset(:,2),'r*')
+    inliers = data(D < d,:);
+    outliers = data(D >= d,:);
 end
 
 function dist = getDist(p1,p2,p3)
@@ -63,7 +83,9 @@ function dist = getDist(p1,p2,p3)
 end
 
 function [ctheta,cr] = cleanData(theta, r)
-    i_clean = r ~= 0; % indices of clean data
+    nonzero_r = r ~= 0;
+    close_r = r < 5;
+    i_clean = nonzero_r & close_r;  % indices of clean data
     ctheta = theta(i_clean);
     cr = r(i_clean);
 end
