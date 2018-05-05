fc20662f375509925838845232649e006ebb3045
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2018-05-05 13:19:41 -0400

    Implement gradient descent of map

--- gauntlet_level_02.m
+++ gauntlet_level_02.m
@@ -6,7 +6,7 @@ function run = runCourse(DRYRUN)
 
     bob_pos = [7, 4.5];
     box_pos = [2 3; 4 4.5; 5.5 2; 0 4.5];
-    walls_pos = [-1,-2, -1,5.5; -1,5.5, 8,5.5; 8,5.5, 8,-1; 8,-1, -1,-2];  % rows of two points defining wall line segments
+    walls_pos = [-1,-1, -1,5.5; -1,5.5, 8,5.5; 8,5.5, 8,-3; 8,-1, -1,-1];  % rows of two points defining wall line segments
     
     run.init.pos = init_pos;
     run.init.head = init_head;
@@ -33,57 +33,90 @@ function run = runCourse(DRYRUN)
     end
     
     % calculate course
-    r = 0.05;  % resolution
-    x = -3:r:10;
-    y = x;
+    r = 0.1;  % resolution
+    x = -10:r:10;
+    y = -10:r:10;
     [X,Y] = meshgrid(x,y);
     % build course
-    %Z = point2field(bob_pos,X,Y,exp(1));
+    Z = point2field(bob_pos,X,Y,exp(1))*10;
     for i = 1:length(box_pos)
-        %Z = Z - point2field(box_pos(i,:),X,Y,exp(1));
+        Z = Z - point2field(box_pos(i,:),X,Y,exp(1));
     end
     % add walls
-    for i = 1:length(walls_pos)
-        p1 = walls_pos(i,1:2);
-        p2 = walls_pos(i,3:4);
-        disp([p1, p2])
-        Z = -line2field(p1,p2,X,Y,r);
-    end
+%     for i = 1:length(walls_pos)
+%         p1 = walls_pos(i,1:2);
+%         p2 = walls_pos(i,3:4);
+%         disp([p1, p2])
+%         Z = Z - line2field(p1,p2,X,Y,r)*100;
+%     end
+    % remove Inf
+%     Z(Z==Inf) = 100;
+%     Z(Z==-Inf) = -100;
+    
     figure;
     surf(X,Y,Z)
     shading interp
-%     ylim([-1 5.5])
-%     xlim([-1 8])
-    %figure;
-    %contour(X,Y,Z,20)
     ylim([-1 5.5])
     xlim([-1 8])
+    figure; hold on
+    contour(X,Y,Z,100)
     
-    [Gx,Gy] = gradient(Z);
-
+    [Gx,Gy] = gradient(Z,r);
+    quiver(X,Y,-Gx,-Gy,10)
+%     hold off
     
-    %% calculate and run path
-    rot = atan2(bob_pos(2),bob_pos(1));
-    dist = getDistance(init_pos,bob_pos);
-
-    % rotate
-    if rot > 0
-        Vr = rs;
-        Vl = -rs;
-    else
-        Vr = -rs;
-        Vl = rs;
+    %% Calculating course
+    a = 0.5;
+    pos(:,1) = [0;0];
+    head(1) = 0;
+    for i = 2:100
+        cpos = pos(:,i-1);
+        xi = find_nearest(cpos(1), X(1,:));  % index of x pos
+        yi = find_nearest(cpos(2), Y(:,1));  % index of y pos
+        g = -[Gx(yi,xi);Gy(yi,xi)];  % flip gradient
+        head(i) = atan2(g(2),g(1));  % preserve direction
+        pos(:,i) = cpos + (g/vecnorm(g)).*a;
     end
-    T = (rot / ((Vr - Vl)/d));
-    drive(Vl,Vr,T)
+    
+    function i = find_nearest(val,range)
+        [v,i] = min(abs(range - val));
+    end
 
-    % advance
-    vs = 0.15*3.2;
-    Vr = vs;
-    Vl = vs;
-    T = (dist / vs);
-    drive(Vl,Vr,T)
-%     setVel(0,0)
+    %% Plot desired path
+%     figure; hold on;
+%     contour(X,Y,Z,100)
+    plot(pos(1,:),pos(2,:),'*-')
+    plot(pos(1,1),pos(2,1),'*g')
+    quiver(pos(1,:),pos(2,:),cos(head),sin(head))
+    title("Desired path")
+    xlabel("X position")
+    ylabel("Y position")
+    ylim([-1 5.5])
+    xlim([-1 8])
+%     axis equal
+
+%     %% calculate and run path
+%     rot = atan2(bob_pos(2),bob_pos(1));
+%     dist = getDistance(init_pos,bob_pos);
+% 
+%     % rotate
+%     if rot > 0
+%         Vr = rs;
+%         Vl = -rs;
+%     else
+%         Vr = -rs;
+%         Vl = rs;
+%     end
+%     T = (rot / ((Vr - Vl)/d));
+%     drive(Vl,Vr,T)
+% 
+%     % advance
+%     vs = 0.15*3.2;
+%     Vr = vs;
+%     Vl = vs;
+%     T = (dist / vs);
+%     drive(Vl,Vr,T)
+% %     setVel(0,0)
 
     %% functions
     function Z = point2field(p1,X,Y, scale)
@@ -96,7 +129,7 @@ function run = runCourse(DRYRUN)
         dx = p2(1) - p1(1);
         num_points = round(getDistance(p1,p2) / r);
         for n = 0:num_points
-           Z = Z + point2field(p1 + [dx,dy]/num_points*n, X, Y, exp(20));
+           Z = Z + point2field(p1 + [dx,dy]/num_points*n, X, Y, exp(1))/num_points;
         end
     end
 
