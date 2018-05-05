0eab6832e45edfd9636f5a9e265b237cd9111e4e
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2018-05-05 17:05:36 -0400

    Layout initial structure

--- gauntlet_level_03.m
+++ gauntlet_level_03.m
@@ -1,18 +1,10 @@
-function run = runCourse(DRYRUN)
-% GAUNTLET_LEVEL_02.m  Run the gauntlet with no obstacles and known BoB
-% DRYRUN: run without connecting to Neato.
-    %% args
-    if nargin < 1
-        DRYRUN = true;
-    end
+function run = runCourse()
+% GAUNTLET_LEVEL_03.m  Run the gauntlet with no obstacles and known BoB
     %% gauntlet info
     init_pos = [0,0];
     init_head = 0;
 
     bob_pos = [7, 4.5];
-    box_pos = [2 3; 4 4.5; 5.5 2];
-    walls_pos = [-1,-1, -1,5.5; -1,5.5, 8,5.5; 8,5.5, 8,-1; 8,-1, -1,-1];  % rows of two points defining wall line segments
-%     walls_pos = [8,5.5,8,-1; 8,-1, -1,-1];
     
     run.init.pos = init_pos;
     run.init.head = init_head;
@@ -26,119 +18,98 @@ function run = runCourse(DRYRUN)
     %% setup
     max_speed = 0.3;
     d = 0.24*3.2;
-    
     rs = 0.15*3.2;
     vs = 0.15*3.2;
     
-    if ~DRYRUN
-        disp("Connecting to robut.")
-        vel_pub = rospublisher('/raw_vel');
-
-        cleanUp = @() stop(vel_pub);  % stop on exit/error
-        cleanupObj = onCleanup(cleanUp);
-    end
-    
-    % calculate course
+    % field parameters
     r = 0.05;  % resolution
     x = -10:r:10;
     y = -10:r:10;
     [X,Y] = meshgrid(x,y);
-    % build course
-    Z = point2field(bob_pos,X,Y,exp(1))*20;
-    for i = 1:length(box_pos)
-        Z = Z - point2field(box_pos(i,:),X,Y,exp(1))*1;
-    end
-    % add walls
-    for i = 1:length(walls_pos(:,1))
-        p1 = walls_pos(i,1:2);
-        p2 = walls_pos(i,3:4);
-        Z = Z - line2field(p1,p2,X,Y,0.5)*1;
-    end
-    % remove Inf
-%     Z(Z==Inf) = 100;
-%     Z(Z==-Inf) = -100;
-
-    figure;
-    surf(X,Y,Z)
-    shading interp
-    ylim([-1 5.5])
-    xlim([-1 8])
-    figure; hold on
-    contour(X,Y,Z,100)
     
-    [Gx,Gy] = gradient(Z,r);
-    quiver(X,Y,-Gx,-Gy,10)
-%     hold off
-    
-    %% Calculating course
+    % path/location parameters
     a = 0.5;  % step size
-    pos(:,1) = [0;0];
-    head(1) = 0;
-    for i = 2:40
-        cpos = pos(:,i-1);
-        xi = find_nearest(cpos(1), X(1,:));  % index of x pos
-        yi = find_nearest(cpos(2), Y(:,1));  % index of y pos
-        g = -[Gx(yi,xi);Gy(yi,xi)];  % flip gradient
-        head(i) = atan2(g(2),g(1));  % preserve direction
-        pos(:,i) = cpos + (g/vecnorm(g)).*a;
-    end
+    pos(:,1) = [0;0];  % initial position
+    head(1) = 0;  % initial heading
     
-    %% Plot desired path
-%     figure; hold on;
-%     contour(X,Y,Z,100)
-    plot(pos(1,:),pos(2,:),'*-')
-    plot(pos(1,1),pos(2,1),'*g')
-    quiver(pos(1,:),pos(2,:),cos(head),sin(head))
-    title("Desired path")
-    xlabel("X position")
-    ylabel("Y position")
-    ylim([-1 5.5])
-    xlim([-1 8])
-%     axis equal
-
-    %% Calculate commands
-    dist = vecnorm(diff(pos,1,2));
-    rot = diff(head);
-    Vr = [];
-    Vl = [];
-    T = [];
-    for i = 1:length(rot)
-        % rotate
-        rs = 0.15*3.2;  % rotation speed
-        if rot(i) > 0
-            Vr = [Vr rs];
-            Vl = [Vl -rs];
-        else
-            Vr = [Vr -rs];
-            Vl = [Vl rs];
+    for j = 1:4
+        % current position = pos(:,end)
+        % current heading = head(end)
+        %% scan lidar
+        walls_pos = lidar_data;  % get line segments in classroom coordinates
+        %% generate field
+        Z = point2field(bob_pos,X,Y,exp(1))*20;  % begin w/ BoB position
+        for i = 1:length(box_pos)
+            Z = Z - point2field(box_pos(i,:),X,Y,exp(1))*1;
         end
-        T = [T (rot(i) / ((Vr(end) - Vl(end))/d))];
-        % advance
-        vs = 0.15*3.2;  % linear velocity speed
-        Vr = [Vr vs];
-        Vl = [Vl vs];
-        T = [T (norm(dist(:,i)) / vs)];
-    end
+        % add walls
+        for i = 1:length(walls_pos(:,1))
+            p1 = walls_pos(i,1:2);
+            p2 = walls_pos(i,3:4);
+            Z = Z - line2field(p1,p2,X,Y,0.5)*1;
+        end
+        %% calculate path
+        for i = length(pos):length(pos) + 5
+            cpos = pos(:,i-1);
+            xi = find_nearest(cpos(1), X(1,:));  % index of x pos
+            yi = find_nearest(cpos(2), Y(:,1));  % index of y pos
+            g = -[Gx(yi,xi);Gy(yi,xi)];  % flip gradient
+            head(i) = atan2(g(2),g(1));  % preserve direction
+            pos(:,i) = cpos + (g/vecnorm(g)).*a;
+        end
+        %% plot path
+        figure; hold on;
+        contour(X,Y,Z,100)
+        plot(pos(1,:),pos(2,:),'*-')
+        plot(pos(1,1),pos(2,1),'*g')
+        quiver(pos(1,:),pos(2,:),cos(head),sin(head))
+        title("Desired path")
+        xlabel("X position")
+        ylabel("Y position")
+        ylim([-1 5.5])
+        xlim([-1 8])
+        hold off;
+        axis equal
+        %% Calculate commands
+        dist = vecnorm(diff(pos,1,2));
+        rot = diff(head);
+        Vr = [];
+        Vl = [];
+        T = [];
+        for i = 1:length(rot)
+            % rotate
+            rs = 0.15*3.2;  % rotation speed
+            if rot(i) > 0
+                Vr = [Vr rs];
+                Vl = [Vl -rs];
+            else
+                Vr = [Vr -rs];
+                Vl = [Vl rs];
+            end
+            T = [T (rot(i) / ((Vr(end) - Vl(end))/d))];
+            % advance
+            vs = 0.15*3.2;  % linear velocity speed
+            Vr = [Vr vs];
+            Vl = [Vl vs];
+            T = [T (norm(dist(:,i)) / vs)];
+        end
+        %% run course
+        fprintf("Max Vr: \t%.3f\n",max(Vr))
+        fprintf("Max Vl: \t%.3f\n",max(Vl))
+        fprintf("Min time: \t%.6f\n",min(T))
+        if min(T) < 0
+            disp("WARNING: Times less than zero")
+        end
+        if max(Vr) > max_speed || max(Vl) > max_speed
+            disp("WARNING: Velocities greater than max of "+string(max_speed)+".")
+        end
 
-    Vl = Vl ./ 3.2;
-    Vr = Vr ./ 3.2;
-
-    %% Run course    
-    fprintf("Max Vr: \t%.3f\n",max(Vr))
-    fprintf("Max Vl: \t%.3f\n",max(Vl))
-    fprintf("Min time: \t%.6f\n",min(T))
-    if min(T) < 0
-        disp("WARNING: Times less than zero")
-    end
-    if max(Vr) > max_speed || max(Vl) > max_speed
-        disp("WARNING: Velocities greater than max of "+string(max_speed)+".")
-    end
-    
-    run.wheel_vel = [Vl',Vr'];
-    run.times = T';
-    % Run course if not a dry run
-    if ~DRYRUN
-        drive(T,Vl,Vr)
+        run.wheel_vel = [Vl',Vr'];
+        run.times = T';
+        % Run course if not a dry run
+        if ~DRYRUN
+            drive(T,Vl,Vr)
+        end
     end
     
     %% functions
